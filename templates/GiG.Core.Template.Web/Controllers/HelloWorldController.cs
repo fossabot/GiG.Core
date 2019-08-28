547936caf33615a7b15c6965a147948cad31948e
Jonathan Busuttil <jonathan.busuttil@gig.com>
2019-08-28 15:14:04 +0200

    Added initial project structure and template.

---
+++ templates/GiG.Core.Template.Web/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GiG.Core.Template.Web.Controllers
+{
+    [ApiController]
+    [Route("[{{ApiName}}]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello World !");
+        }
+    }
+}
