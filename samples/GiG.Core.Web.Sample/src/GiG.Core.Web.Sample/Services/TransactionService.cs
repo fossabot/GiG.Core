c259eb48f976cf75a466e529bec0caa7b69bad6c
david.phyall <david.phyall@igamingcloud.com>
2019-08-29 15:26:33 +0200

    Created sample web api using logging

---
+++ samples/GiG.Core.Web.Sample/src/GiG.Core.Web.Sample/Services/TransactionService.cs
@@ -0,0 +1,36 @@
+﻿using GiG.Core.Web.Sample.Contracts;
+using Microsoft.Extensions.Logging;
+
+namespace GiG.Core.Web.Sample.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private decimal _playerBalance {get;set;}
+        private ILogger _logger { get; set; }
+
+        public TransactionService(ILogger<TransactionService> logger)
+        {
+            _logger = logger;
+        }
+
+        public decimal GetBalance()
+        {
+            return _playerBalance;
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            _logger.LogInformation($"Deposit {amount}");
+            _playerBalance += amount;
+            return GetBalance();
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            _logger.LogInformation($"Withdraw {amount}");
+            _playerBalance -= amount;
+            return GetBalance();
+        }
+
+    }
+}
