799c6132fc820bc108e661dbde44b2c3b23d943f
Jessica Buttigieg <jessica.buttigieg@gig.com>
2019-08-30 10:26:26 +0200

    Web Sample steps

--- samples/GiG.Core.Web.Sample/tests/component/GiG.Core.Web.Sample.Tests.Component/Services/WebSampleService.cs
+++ samples/GiG.Core.Web.Sample/tests/component/GiG.Core.Web.Sample.Tests.Component/Services/WebSampleService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GiG.Core.Web.Sample.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,30 @@ namespace GiG.Core.Web.Sample.Tests.Component.Services
 {
     public class WebSampleService
     {
+        private const string MicroserviceUrl = "http://localhost:5000/transactions";
 
+
+        public decimal GetBalance()
+        {
+            return 0;
+        }
+
+        public decimal Deposit(TransactionRequest txRequest) 
+        {
+            return 0;
+        }
+
+        public decimal Withdraw(TransactionRequest txRequest)
+        {
+            return 0;
+        }
+
+        public decimal GetMinimumDepositLimit()
+        {
+            return 0;
+        }
+
+
+        
     }
 }
--- samples/GiG.Core.Web.Sample/tests/component/GiG.Core.Web.Sample.Tests.Component/StepDefinitions/WebSampleSteps.cs
+++ samples/GiG.Core.Web.Sample/tests/component/GiG.Core.Web.Sample.Tests.Component/StepDefinitions/WebSampleSteps.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GiG.Core.Web.Sample.Tests.Component.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using TechTalk.SpecFlow;
@@ -8,15 +9,45 @@ namespace GiG.Core.Web.Sample.Tests.Component.StepDefinitions
     [Binding]
     public class WebSampleSteps
     {
+        public WebSampleService _webSampleService;
+
         public WebSampleSteps() 
-        { 
+        {
+            
         }
 
         [Given(@"I get the current balance of the player")]
         public void GivenIGetTheCurrentBalanceOfThePlayer()
         {
-            
+            WebSampleService webSampleService = new WebSampleService();
+
+            decimal balance = webSampleService.GetBalance();
         }
+
+        [Given(@"I deposit '(.*)'")]
+        public void GivenIDeposit(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I get the new balance of the player")]
+        public void WhenIGetTheNewBalanceOfThePlayer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the balance should be updated correctly")]
+        public void ThenTheBalanceShouldBeUpdatedCorrectly()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I withdraw '(.*)'")]
+        public void WhenIWithdraw(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
 
     }
 }
