0c6dee2ed40834ac128ee9f832cfc7d368b1dff3
Jonathan Busuttil <jonathan.busuttil@gig.com>
2019-09-03 09:41:36 +0200

    Added HealthCheck integration test project

---
+++ tests/integration/GiG.Core.Extensions.HealthCheck.Tests.Integration/HealthCheckTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace GiG.Core.Extensions.HealthCheck.Tests.Integration
+{
+    public class HealthCheckTests
+    {
+        private readonly TestServer _testServer;
+
+        public HealthCheckTests()
+        {
+            _testServer = new TestServer(new WebHostBuilder().UseStartup<MockStartup>());
+        }
+
+
+    }
+
+    public class MockStartup
+    {
+        [Fact]
+        public async Task RespondWithHealthyStatusOnLiveHealthCheck()
+        {
+
+        }
+
+        [Fact]
+        public async Task RespondWithHealthyStatusOnReadyHealthCheck()
+        {
+
+        }
+    }
+}
