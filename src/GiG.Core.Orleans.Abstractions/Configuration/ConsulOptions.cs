9c6fc5278de923a9af49100fc3ee622438cccb71
Clinton <clinton.mifsud@gig.com>
2019-09-11 18:25:57 +0200

    Initial Commit For Orleans and Consul

---
+++ src/GiG.Core.Orleans.Abstractions/Configuration/ConsulOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace GiG.Core.Orleans.Abstractions.Configuration
+{
+    /// <summary>
+    /// Orleans Consul Settings
+    /// </summary>
+    public class ConsulOptions
+    {
+        /// <summary>
+        /// Default configuration section name.
+        /// </summary>
+        public const string DefaultConfigurationSection = "Orleans:Consul";
+      
+        /// <summary>
+        /// Consul Address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// KV Root Folder
+        /// </summary>
+        public string KvRootFolder { get; set; }
+    }
+}
---
+++ src/GiG.Core.Orleans.Clustering.Consul/Extensions/SiloBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using GiG.Core.Orleans.Abstractions.Configuration;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+using Orleans;
+using Orleans.Hosting;
+using System;
+
+namespace GiG.Core.Orleans.Clustering.Consul.Extensions
+{
+    public static class SiloBuilderExtensions
+    {
+        /// <summary>
+        /// Configures Consul in Orleans.
+        /// </summary>
+        /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
+        /// <param name="configurationSection"></param>
+        /// <returns>Returns the <see cref="ISiloBuilder"/> so that more methods can be chained.</returns>
+        public static ISiloBuilder ConfigureConsul([NotNull] this ISiloBuilder builder, [NotNull] IConfigurationSection configurationSection)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (configurationSection == null) throw new ArgumentNullException(nameof(configurationSection));
+
+            builder.Configure<ConsulOptions>(configurationSection);
+
+            var consulOptions = configurationSection.Get<ConsulOptions>() ?? new ConsulOptions();
+
+            return
+                  builder.UseConsulClustering((options) =>
+                  {
+                      options.Address = new Uri(consulOptions.Address);
+                      options.KvRootFolder = consulOptions.KvRootFolder;
+                  });                
+        }
+    }
+}
