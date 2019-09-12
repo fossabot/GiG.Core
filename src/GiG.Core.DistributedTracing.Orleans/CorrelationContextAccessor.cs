5c05cd07876a6e2b1a78e25847efeca68ac4f6f0
Jonathan Busuttil <jonathan.busuttil@gig.com>
2019-09-12 09:49:18 +0200

    Added Correlation Context Accessor for Orleans.

---
+++ src/GiG.Core.DistributedTracing.Orleans/CorrelationContextAccessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GiG.Core.DistributedTracing.Abstractions;
+using Orleans.Runtime;
+
+namespace GiG.Core.DistributedTracing.Orleans
+{
+    public class CorrelationContextAccessor : ICorrelationContextAccessor
+    {
+        public Guid Value => 
+            Guid.TryParse(RequestContext.Get(Constants.Header)?.ToString(), out var correlationId)
+                ? correlationId
+                : Guid.Empty;
+    }
+}
