8347417b7ea38961b160ffd95599ec645fb62071
Jonathan Busuttil <jonathan.busuttil@gig.com>
2019-09-12 10:25:29 +0200

    Added GrainCallFilter for adding Correlation Id Header in Orleans.

---
+++ src/GiG.Core.DistributedTracing.Orleans/CorrelationGrainCallFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using GiG.Core.DistributedTracing.Abstractions;
+using Orleans;
+using Orleans.Runtime;
+
+namespace GiG.Core.DistributedTracing.Orleans
+{
+    public class CorrelationGrainCallFilter : IOutgoingGrainCallFilter
+    {
+        private readonly ICorrelationContextAccessor _correlationContextAccessor;
+
+        public CorrelationGrainCallFilter(ICorrelationContextAccessor correlationContextAccessor)
+        {
+            _correlationContextAccessor = correlationContextAccessor;
+        }
+
+        public async Task Invoke(IOutgoingGrainCallContext context)
+        {
+            RequestContext.Set(Constants.Header, _correlationContextAccessor?.Value);
+            await context.Invoke();
+        }
+    }
+}
