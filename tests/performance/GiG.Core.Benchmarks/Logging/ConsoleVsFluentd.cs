a0dab2b9fb389cf02414431a6bce7065dc83ce10
David Anastasi <david.anastasi@igamingcloud.com>
2019-09-23 15:10:53 +0200

    Merged in QAs Logging benchmarks

---
+++ tests/performance/GiG.Core.Benchmarks/Logging/ConsoleVsFluentd.cs
@@ -0,0 +1,65 @@
+﻿using BenchmarkDotNet.Attributes;
+using GiG.Core.Logging.All.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GiG.Core.Benchmarks.Logging
+{
+    [BenchmarkCategory("Logging")]
+    public class ConsoleVsFluentd
+    {
+        private IHost _consoleHost;
+        private IHost _fluentDHost;
+
+        [Params(1, 30, 500, 1000)]
+        public int LogCount { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _consoleHost = new HostBuilder()
+                .ConfigureHostConfiguration(builder => builder
+                    .AddJsonFile("appsettings.consolelogger.json"))
+                .ConfigureLogging()
+                .Build();
+
+            _fluentDHost = new HostBuilder()
+                .ConfigureHostConfiguration(builder => builder
+                    .AddJsonFile("appsettings.fluentdlogger.json"))
+                .ConfigureLogging()
+                .Build();
+        }
+
+        [Benchmark]
+        public void LogToConsole()
+        {
+            var consoleLogger = _consoleHost.Services.GetRequiredService<ILogger<ConsoleVsFluentd>>();
+
+            // Log to console
+            for (var i = 0; i < LogCount; i++)
+            {
+                consoleLogger.LogInformation("Test Console");
+            }
+        }
+
+        [Benchmark]
+        public void LogToFluentD()
+        {
+            var fluentDLogger = _fluentDHost.Services.GetRequiredService<ILogger<ConsoleVsFluentd>>();
+
+            // Log to fluentd
+            for (var i = 0; i < LogCount; i++)
+            {
+                fluentDLogger.LogInformation("Test Fluentd");
+            }
+        }
+
+        [GlobalCleanup]
+        public void TearDown()
+        {
+
+        }
+    }
+}
---
+++ tests/performance/GiG.Core.Benchmarks/Logging/WithinVsOutsideLogLevel.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+using GiG.Core.Logging.All.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GiG.Core.Benchmarks.Logging
+{
+    [BenchmarkCategory("Logging")]
+    public class WithinVsOutsideLogLevel
+    {
+        public IHost ConsoleHost { get; private set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            ConsoleHost = new HostBuilder()
+                .ConfigureHostConfiguration(builder => builder
+                    .AddJsonFile("appsettings.minimumloglevel.json"))
+                .ConfigureLogging()
+                .Build();
+        }
+
+        [Benchmark]
+        public void LogOutsideLevel()
+        {
+            var consoleLogger = ConsoleHost.Services.GetRequiredService<ILogger<WithinVsOutsideLogLevel>>();
+
+            // Log to console
+            for (var i = 0; i < 1000; i++)
+            {
+                consoleLogger.LogInformation("Test Outside Level");
+            }
+        }
+
+        [Benchmark]
+        public void LogWithinLevel()
+        {
+            var consoleLogger = ConsoleHost.Services.GetRequiredService<ILogger<WithinVsOutsideLogLevel>>();
+
+            // Log to console
+            for (var i = 0; i < 1000; i++)
+            {
+                consoleLogger.LogError("Test Within Level");
+            }
+        }
+
+        [GlobalCleanup]
+        public void TearDown()
+        {
+
+        }
+    }
+}
