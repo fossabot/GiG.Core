df86857aefde795365a33c061ce5baa7997fb886
Jonathan Busuttil <jonathan.busuttil@gig.com>
2019-10-02 14:11:50 +0200

    Added benchmarks for MasterMemory Cache read operation for small and large datasets while also comparing to reading large datasets from a Hashset in memory

---
+++ tests/performance/GiG.Core.Benchmarks/Cache/MasterMemoryLargeCacheBenchmarks.cs
@@ -0,0 +1,73 @@
+﻿using BenchmarkDotNet.Attributes;
+using GiG.Core.Benchmarks.Cache.Mocks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GiG.Core.Benchmarks.Cache
+{
+    [MemoryDiagnoser]
+    public class MasterMemoryLargeCacheBenchmarks
+    {
+        private static MemoryDatabase _cache;
+        private static List<PasswordBlacklist> _searchList;
+        
+        [Params(10_000_000)]
+        public int ReadCount { get; set; }
+
+        private int _searchListCount = 1_000_000;
+
+        public MasterMemoryLargeCacheBenchmarks()
+        {
+            _cache = BuildCache();
+        }
+        
+        [Benchmark]
+        public void ReadOperations_SmallDataSet_LargeReadCount()
+        {            
+            var random = new Random();
+
+            for (var i = 0; i <= ReadCount; i++)
+            {
+                var index = random.Next(1, _searchListCount);
+
+                var searchTerm = _searchList[index];
+
+                _cache.PasswordBlacklistTable.FindByValue(searchTerm.Value);                
+            }
+        }
+
+        private MemoryDatabase BuildCache()
+        {
+            var builder = new DatabaseBuilder();
+
+            var passwordEntries = File.ReadAllLines("Cache\\Mocks\\passwordblacklist.txt");
+
+            var passwordBlackist = new List<PasswordBlacklist>();
+
+            foreach (var entry in passwordEntries)
+            {
+                passwordBlackist.Add(new PasswordBlacklist { Value = entry });
+            }
+
+            var random = new Random();
+
+            _searchList = new List<PasswordBlacklist>();
+
+            for (var i = 0; i <= _searchListCount; i++)
+            {
+                var index = random.Next(1, _searchListCount);
+
+                var entry = passwordBlackist[index];
+
+                _searchList.Add(entry);
+            }            
+
+            builder.Append(passwordBlackist);
+
+            byte[] data = builder.Build();
+
+            return new MemoryDatabase(data);
+        }
+    }
+}
