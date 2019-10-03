f08474e4a4157c1c2a34f406cd42047e6ce1a7e7
David Anastasi <david.anastasi@igamingcloud.com>
2019-10-03 17:22:35 +0200

    Fixed benchmark method names, and cleaned up a bit.

--- tests/benchmarks/GiG.Core.Benchmarks/Cache/HashsetBenchmarks.cs
+++ tests/benchmarks/GiG.Core.Benchmarks/Cache/HashsetBenchmarks.cs
@@ -15,23 +15,19 @@ namespace GiG.Core.Benchmarks.Cache
         [Params(10_000_000)]
         public int ReadCount { get; set; }
 
-        private int _searchListCount = 1_000_000;
+        private const int SearchListCount = 1_000_000;
 
-        public HashsetBenchmarks()
-        {
-            _cache = BuildCache();
-        }
+        public HashsetBenchmarks() => _cache = BuildCache();
 
         [Benchmark]
-        public void LargeDataSet_LargeReadCount_ItemFound()
+        public void HashSets_LargeDataSet_ItemFound()
         {
             var random = new Random();
 
             for (var i = 0; i <= ReadCount; i++)
             {
-                ///we pick a random item from the search list to use it as a search term to search the hashset with
-                var index = random.Next(1, _searchListCount);
-
+                // pick a random item from the search list to use it as a search term to search the hashset with
+                var index = random.Next(1, SearchListCount);
                 var searchTerm = _searchList[index];
 
                 _cache.Contains(searchTerm);
@@ -39,48 +35,46 @@ namespace GiG.Core.Benchmarks.Cache
         }
 
         [Benchmark]
-        public void LargeDataSet_LargeReadCount_ItemNotFound()
+        public void HashSets_LargeDataSet_ItemNotFound()
         {
             var random = new Random();
 
             for (var i = 0; i <= ReadCount; i++)
             {
-                ///we pick a random item from the search list to use it as a search term to search the hashset with
-                var index = random.Next(1, _searchListCount);
-
+                // pick a random item from the search list to use it as a search term to search the hashset with
+                var index = random.Next(1, SearchListCount);
                 var searchTerm = _searchList[index];
 
-                searchTerm.Value = string.Format("{0} + 3fn89r", searchTerm.Value);
+                searchTerm.Value = $"{searchTerm.Value} + 3fn89r";
 
                 _cache.Contains(searchTerm);
             }
         }
 
-        private HashSet<PasswordBlacklist> BuildCache()
+        private static HashSet<PasswordBlacklist> BuildCache()
         {          
             var passwordEntries = File.ReadAllLines("Cache\\Mocks\\passwordblacklist.txt");
 
-            var passwordBlackist = new List<PasswordBlacklist>();
+            var passwordBlacklist = new List<PasswordBlacklist>();
             var result = new HashSet<PasswordBlacklist>();
 
             foreach (var entry in passwordEntries)
             {
-                var passwordBlackistEntry = new PasswordBlacklist { Value = entry };
+                var passwordBlacklistEntry = new PasswordBlacklist { Value = entry };
 
-                result.Add(passwordBlackistEntry);
-                passwordBlackist.Add(passwordBlackistEntry);
+                result.Add(passwordBlacklistEntry);
+                passwordBlacklist.Add(passwordBlacklistEntry);
             }
 
             var random = new Random();
 
-            //we build a search list by picking one million random values 
+            // build a search list by picking one million random values 
             _searchList = new List<PasswordBlacklist>();
 
-            for (var i = 0; i <= _searchListCount; i++)
+            for (var i = 0; i <= SearchListCount; i++)
             {
-                var index = random.Next(1, _searchListCount - 10);
-
-                var entry = passwordBlackist[index];
+                var index = random.Next(1, SearchListCount - 10);
+                var entry = passwordBlacklist[index];
 
                 _searchList.Add(entry);
             }
