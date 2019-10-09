26f62308e41b2893009dcc41901eed2a1fdc9446
David Anastasi <david.anastasi@igamingcloud.com>
2019-10-09 10:52:19 +0200

    Created CorrelationIdDelegatingHandler and TenantDelegatingHandler.

---
+++ src/GiG.Core.Http.DistributedTracing/CorrelationIdDelegatingHandler.cs
@@ -0,0 +1,32 @@
+﻿using GiG.Core.DistributedTracing.Abstractions;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GiG.Core.Http.DistributedTracing
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CorrelationIdDelegatingHandler : DelegatingHandler
+    {
+        private readonly ICorrelationContextAccessor _correlationContextAccessor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="correlationContextAccessor"></param>
+        public CorrelationIdDelegatingHandler(ICorrelationContextAccessor correlationContextAccessor) => _correlationContextAccessor = correlationContextAccessor;
+
+        /// <inheritdoc />
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains(Constants.Header))
+            {
+                request.Headers.Add(Constants.Header, _correlationContextAccessor.Value.ToString());
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ src/GiG.Core.Http.MultiTenant/TenantDelegatingHandler.cs
@@ -0,0 +1,32 @@
+﻿using GiG.Core.MultiTenant.Abstractions;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GiG.Core.Http.MultiTenant
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TenantDelegatingHandler : DelegatingHandler
+    {
+        private readonly ITenantAccessor _tenantAccessor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tenantAccessor"></param>
+        public TenantDelegatingHandler(ITenantAccessor tenantAccessor) => _tenantAccessor = tenantAccessor;
+
+        /// <inheritdoc />
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains(Constants.Header))
+            {
+                request.Headers.Add(Constants.Header, _tenantAccessor.Values);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
