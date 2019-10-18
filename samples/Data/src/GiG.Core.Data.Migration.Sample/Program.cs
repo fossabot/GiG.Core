5a713bf65cd87e1f7c9f57904db84caefdb826dd
David Anastasi <david.anastasi@igamingcloud.com>
2019-10-18 10:02:18 +0200

    Removed VersionPrefix from Directory.Build.props
    Fixed Data Migration Program.cs

--- samples/Data/src/GiG.Core.Data.Migration.Sample/Program.cs
+++ samples/Data/src/GiG.Core.Data.Migration.Sample/Program.cs
@@ -1,20 +1,42 @@
 ﻿using GiG.Core.Logging.All.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace GiG.Core.Data.Migration.Sample
 {
-    internal static class Program
+    internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task<int> Main(string[] args)
         {
-            CreateHostBuilder().Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            
+            using (host)
+            {
+                try
+                {
+                    await host.StartAsync();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex.Message, ex);
+                    return 1;
+                }
+                finally
+                {
+                    await host.StopAsync();
+                }
+
+                return 0;
+            }
         }
 
-        private static IHostBuilder CreateHostBuilder()
-        {
-            return Host.CreateDefaultBuilder()
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
                 .ConfigureLogging()
                 .ConfigureServices(Startup.ConfigureServices);
-        }
     }
 }
