de5b85a3598a098243eb8eb705262b734cf25eab
david.anastasi <david.anastasi@gig.com>
2019-10-19 21:46:26 +0200

    Added documentation for Configuration, DistributedTracing.Orleans, Logging.Enrichers.*
    Updated parent README.md
    Added missing XML comments to public methods/properties.

--- src/GiG.Core.Hosting.Abstractions/InfoManagementOptions.cs
+++ src/GiG.Core.Hosting.Abstractions/InfoManagementOptions.cs
@@ -1,11 +1,23 @@
 ﻿namespace GiG.Core.Hosting.Abstractions
 {
+    /// <summary>
+    /// Info Management Options
+    /// </summary>
     public class InfoManagementOptions
     {
+        /// <summary>
+        /// Info Management default section name.
+        /// </summary>
         public const string DefaultSectionName = "InfoManagement";
 
+        /// <summary>
+        /// Indicates whether InfoManagement is enabled or not.
+        /// </summary>
         public bool IsEnabled { get; set; } = true;
 
+        /// <summary>
+        /// The InfoManagement Url.
+        /// </summary>
         public string Url { get; set; } = "/actuator/info";
     }
 }
