dedd11747115dd0e2f1558ee830fe5379d5557be
David Anastasi <david.anastasi@igamingcloud.com>
2019-11-09 08:06:40 +0100

    Added tests that Assert ArgumentNullException is thrown.

---
+++ tests/integration/GiG.Core.HealthChecks.Tests.Integration/Tests/HealthChecksBuilderExtensionsTests.cs
@@ -0,0 +1,81 @@
+﻿using GiG.Core.HealthChecks.Extensions;
+using GiG.Core.HealthChecks.Tests.Integration.Mocks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.HealthChecks.Tests.Integration.Tests
+{
+    [Trait("Category", "Integration")]
+    public class HealthChecksBuilderExtensionsTests
+    {
+        [Fact]
+        public void AddReadyCheck_HealthChecksBuilderIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => HealthChecksBuilderExtensions.AddReadyCheck<CachedUnHealthyCheck>(null, ""));
+            Assert.Throws<ArgumentNullException>(() => HealthChecksBuilderExtensions.AddReadyCheck(null, "", null));
+        }
+
+        [Fact]
+        public void AddReadyCheck_HealthCheckNameIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TestServer(
+                new WebHostBuilder().UseStartup<MockStartupWithDefaultConfiguration>()
+                    .ConfigureServices(x => x
+                        .AddCachedHealthChecks()
+                        .AddReadyCheck<CachedUnHealthyCheck>(null))));
+
+            Assert.Throws<ArgumentNullException>(() => new TestServer(
+                new WebHostBuilder().UseStartup<MockStartupWithDefaultConfiguration>()
+                    .ConfigureServices(x => x
+                        .AddCachedHealthChecks()
+                        .AddReadyCheck(null, null))));
+        }
+
+        [Fact]
+        public void AddReadyCheck_InstanceIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TestServer(
+                new WebHostBuilder().UseStartup<MockStartupWithDefaultConfiguration>()
+                    .ConfigureServices(x => x
+                        .AddCachedHealthChecks()
+                        .AddReadyCheck(nameof(CachedUnHealthyCheck), null, HealthStatus.Unhealthy))));
+        }
+
+        [Fact]
+        public void AddLiveCheck_HealthChecksBuilderIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => HealthChecksBuilderExtensions.AddLiveCheck<CachedUnHealthyCheck>(null, ""));
+            Assert.Throws<ArgumentNullException>(() => HealthChecksBuilderExtensions.AddLiveCheck(null, "", null));
+        }
+
+        [Fact]
+        public void AddLiveCheck_HealthCheckNameIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TestServer(
+                new WebHostBuilder().UseStartup<MockStartupWithDefaultConfiguration>()
+                    .ConfigureServices(x => x
+                        .AddCachedHealthChecks()
+                        .AddLiveCheck<CachedUnHealthyCheck>(null))));
+
+            Assert.Throws<ArgumentNullException>(() => new TestServer(
+                new WebHostBuilder().UseStartup<MockStartupWithDefaultConfiguration>()
+                    .ConfigureServices(x => x
+                        .AddCachedHealthChecks()
+                        .AddLiveCheck(null, null))));
+        }
+
+        [Fact]
+        public void AddLiveCheck_InstanceIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TestServer(
+                new WebHostBuilder().UseStartup<MockStartupWithDefaultConfiguration>()
+                    .ConfigureServices(x => x
+                        .AddCachedHealthChecks()
+                        .AddLiveCheck(nameof(CachedUnHealthyCheck), null, HealthStatus.Unhealthy))));
+        }
+    }
+}
---
+++ tests/integration/GiG.Core.Http.Tests.Integration/Tests/HttpClientExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using GiG.Core.Http.Extensions;
+using System;
+using System.Net.Http;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.Http.Tests.Integration.Tests
+{
+    [Trait("Category", "Integration")]
+    public class HttpClientExtensionsTests
+    {
+        [Fact]
+        public void FromConfiguration_HttpClientIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => HttpClientExtensions.FromConfiguration(null, null, ""));
+            Assert.Throws<ArgumentNullException>(() => HttpClientExtensions.FromConfiguration(null, "", null));
+        }
+
+        [Fact]
+        public void FromConfiguration_ConfigurationIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpClient().FromConfiguration(null, ""));
+        }
+
+        [Fact]
+        public void FromConfiguration_ConfigurationSectionIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpClient().FromConfiguration("", null));
+        }
+    }
+}
