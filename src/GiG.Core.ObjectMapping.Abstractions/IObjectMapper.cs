c5c09c0e6fd85bc7432adebbfc165efd8918a05b
david.anastasi <david.anastasi@gig.com>
2019-11-11 22:25:46 +0100

    Added new projects GiG.Core.ObjectMapping.AutoMapper and GiG.Core.ObjectMapping.Abstractions.
    Added IObjectMapper, and implemented 7 AutoMapper methods.
    Added ServiceCollectionExtensions to register AutoMapper with it's profiles, and add IObjectMapper to the ServiceCollection.

---
+++ src/GiG.Core.ObjectMapping.Abstractions/IObjectMapper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace GiG.Core.ObjectMapping.Abstractions
+{
+    /// <summary>
+    /// Object Mapper.
+    /// </summary>
+    public interface IObjectMapper
+    {
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object.
+        /// The source type is inferred from the source object.
+        /// </summary>
+        /// <typeparam name="TDestination">Destination type to create</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <returns>Mapped destination object</returns>
+        TDestination Map<TDestination>(object source);
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object.
+        /// </summary>
+        /// <typeparam name="TSource">Source type to use, regardless of the runtime type</typeparam>
+        /// <typeparam name="TDestination">Destination type to create</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <returns>Mapped destination object</returns>
+        TDestination Map<TSource, TDestination>(TSource source);
+
+        /// <summary>
+        /// Execute a mapping from the source object to the existing destination object.
+        /// </summary>
+        /// <typeparam name="TSource">Source type to use</typeparam>
+        /// <typeparam name="TDestination">Destination type</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="destination">Destination object to map into</param>
+        /// <returns>The mapped destination object, same instance as the <paramref name="destination"/> object</returns>
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object with explicit <see cref="System.Type"/> objects
+        /// </summary>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="sourceType">Source type to use</param>
+        /// <param name="destinationType">Destination type to create</param>
+        /// <returns>Mapped destination object</returns>
+        object Map(object source, Type sourceType, Type destinationType);
+
+        /// <summary>
+        /// Execute a mapping from the source object to existing destination object with explicit <see cref="System.Type"/> objects
+        /// </summary>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="destination">Destination object to map into</param>
+        /// <param name="sourceType">Source type to use</param>
+        /// <param name="destinationType">Destination type to use</param>
+        /// <returns>Mapped destination object, same instance as the <paramref name="destination"/> object</returns>
+        object Map(object source, object destination, Type sourceType, Type destinationType);
+
+        /// <summary>
+        /// Project the input queryable.
+        /// </summary>
+        /// <remarks>Projections are only calculated once and cached</remarks>
+        /// <typeparam name="TDestination">Destination type</typeparam>
+        /// <param name="source">Queryable source</param>
+        /// <param name="parameters">Optional parameter object for parameterized mapping expressions</param>
+        /// <param name="membersToExpand">Explicit members to expand</param>
+        /// <returns>Queryable result, use queryable extension methods to project and execute result</returns>
+        IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source, object parameters = null, params Expression<Func<TDestination, object>>[] membersToExpand);
+
+        /// <summary>
+        /// Project the input queryable.
+        /// </summary>
+        /// <typeparam name="TDestination">Destination type to map to</typeparam>
+        /// <param name="source">Queryable source</param>
+        /// <param name="parameters">Optional parameter object for parameterized mapping expressions</param>
+        /// <param name="membersToExpand">Explicit members to expand</param>
+        /// <returns>Queryable result, use queryable extension methods to project and execute result</returns>
+        IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source, IDictionary<string, object> parameters, params string[] membersToExpand);
+    }
+}
---
+++ src/GiG.Core.ObjectMapping.AutoMapper/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using IObjectMapper = GiG.Core.ObjectMapping.Abstractions.IObjectMapper;
+
+namespace GiG.Core.ObjectMapping.AutoMapper.Extensions
+{
+    /// <summary>
+    /// Service Collection Extensions.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Configure IObjectMapper to use AutoMapper.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="assemblies">The Assemblies where the Automapper profiles are located.</param>
+        /// <param name="serviceLifetime">Specifies the lifetime of the services in the <see cref="IServiceCollection"/>.</param>
+        public static void AddObjectMapper(this IServiceCollection services, IEnumerable<Assembly> assemblies, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+        {
+            services.AddAutoMapper(assemblies, serviceLifetime);
+            services.TryAddScoped<IObjectMapper, AutoMapperObjectMapper>();
+        }
+
+        /// <summary>
+        /// Configure IObjectMapper to use AutoMapper.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="profileAssemblyMarkerTypes">Marker types which tell AutoMapper where the profiles are located.</param>
+        public static void AddObjectMapper(this IServiceCollection services, params Type[] profileAssemblyMarkerTypes)
+        {
+            services.AddAutoMapper(profileAssemblyMarkerTypes);
+            services.TryAddScoped<IObjectMapper, AutoMapperObjectMapper>();
+        }
+    }
+}
