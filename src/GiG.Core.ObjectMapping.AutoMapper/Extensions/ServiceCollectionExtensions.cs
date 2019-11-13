de47b9b59d51e68d3dbe411585ef817172bac6d2
David Anastasi <david.anastasi@igamingcloud.com>
2019-11-13 10:04:18 +0100

    .

--- src/GiG.Core.ObjectMapping.AutoMapper/Extensions/ServiceCollectionExtensions.cs
+++ src/GiG.Core.ObjectMapping.AutoMapper/Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
 using IObjectMapper = GiG.Core.ObjectMapping.Abstractions.IObjectMapper;
 
@@ -17,22 +16,10 @@ namespace GiG.Core.ObjectMapping.AutoMapper.Extensions
         /// Configure IObjectMapper to use AutoMapper.
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-        /// <param name="assemblies">The Assemblies where the Automapper profiles are located.</param>
+        /// <param name="assemblies">The Assemblies where the AutoMapper profiles are located.</param>
         public static void AddObjectMapper(this IServiceCollection services, params Assembly[] assemblies)
         {
             services.AddAutoMapper(assemblies);
-            services.TryAddScoped<IObjectMapper, AutoMapperObjectMapper>();
-        }
-
-        /// <summary>
-        /// Configure IObjectMapper to use AutoMapper.
-        /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-        /// <param name="assemblies">The Assemblies where the Automapper profiles are located.</param>
-        /// <param name="serviceLifetime">Specifies the lifetime of the services in the <see cref="IServiceCollection"/>.</param>
-        public static void AddObjectMapper(this IServiceCollection services, IEnumerable<Assembly> assemblies, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
-        {
-            services.AddAutoMapper(assemblies, serviceLifetime);
             services.TryAddScoped<IObjectMapper, AutoMapperObjectMapper>();
         }
 
