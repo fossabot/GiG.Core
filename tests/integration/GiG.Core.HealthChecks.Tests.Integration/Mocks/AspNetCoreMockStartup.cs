a8b7c5de98605266126208427a5472e409fca352
David Anastasi <david.anastasi@igamingcloud.com>
2019-11-14 16:15:57 +0100

    Added integration tests.

---
+++ tests/integration/GiG.Core.HealthChecks.Tests.Integration/Mocks/AspNetCoreMockStartup.cs
@@ -0,0 +1,31 @@
+﻿using GiG.Core.HealthChecks.AspNetCore.Extensions;
+using GiG.Core.Web.Mock;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GiG.Core.HealthChecks.Tests.Integration.Mocks
+{
+    internal class AspNetCoreMockStartup : MockStartupBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AspNetCoreMockStartup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+            base.ConfigureServices(services);
+        }
+
+        /// <inheritdoc />
+        public override void Configure(IApplicationBuilder app)
+        {
+            base.Configure(app);
+            app.UseEndpoints(endpoints => { endpoints.MapHealthChecks(); });
+        }
+    }
+}
