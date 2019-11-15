d9ecb3ec789e3d18c6a43e83bbac8942f2a5412a
david.phyall <david.phyall@igamingcloud.com>
2019-11-15 09:37:40 +0100

    Re-added the readme.

---
+++ tests/performance/GiG.Core.Web.FluentValidation.Tests.Performance/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace GiG.Core.Web.FluentValidation.Tests.Performance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<JsonWriterTest>(); 
+
+        }
+    }
+}
