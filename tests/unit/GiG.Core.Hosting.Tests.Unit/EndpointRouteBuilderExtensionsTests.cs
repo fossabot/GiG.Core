d441e808cd10009fe3896d145d7199b11835f6d1
david.anastasi <david.anastasi@gig.com>
2019-11-15 14:23:28 +0100

    Added further tests.

---
+++ tests/unit/GiG.Core.Hosting.Tests.Unit/EndpointRouteBuilderExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using GiG.Core.Hosting.AspNetCore.Extensions;
+using System;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.Hosting.Tests.Unit
+{
+    [Trait("Category", "Unit")]
+    public class EndpointRouteBuilderExtensionsTests
+    {
+        [Fact]
+        public void MapInfoManagement_EndpointRouteBuilderIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => EndpointRouteBuilderExtensions.MapInfoManagement(null));
+        }
+    }
+}
---
+++ tests/unit/GiG.Core.Logging.Tests.Unit/HostBuilderExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using GiG.Core.Logging.Extensions;
+using Microsoft.Extensions.Hosting;
+using System;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.Logging.Tests.Unit
+{
+    [Trait("Category", "Unit")]
+    public class HostBuilderExtensionsTests
+    {
+        [Fact]
+        public void ConfigureLogging_HostBuilderIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => HostBuilderExtensions.ConfigureLogging(null, null));
+            Assert.Throws<ArgumentNullException>(() => All.Extensions.HostBuilderExtensions.ConfigureLogging(null, null));
+        }
+
+        [Fact]
+        public void ConfigureLogging_SectionNameIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HostBuilder().ConfigureLogging(null, null));
+        }
+    }
+}
