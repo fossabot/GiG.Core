8f0aafd18902eceb524da922fe023d57ab0e2974
David Anastasi <david.anastasi@igamingcloud.com>
2019-11-19 11:59:04 +0100

    .

---
+++ tests/unit/GiG.Core.Web.Tests.Unit/Hosting/ApplicationBuilderExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using GiG.Core.Web.Hosting.Extensions;
+using Microsoft.AspNetCore.Builder;
+using System;
+using Xunit;
+using ApplicationBuilderExtensions = GiG.Core.Web.Hosting.Extensions.ApplicationBuilderExtensions;
+
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.Web.Tests.Unit.Hosting
+{
+    [Trait("Category", "Unit")]
+    public class ApplicationBuilderExtensionsTests
+    {
+        [Fact]
+        public void UsePathBaseFromConfiguration_ApplicationBuilderIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => ApplicationBuilderExtensions.UsePathBaseFromConfiguration(null));
+            Assert.Equal("builder", exception.ParamName);
+
+            exception = Assert.Throws<ArgumentNullException>(() => ApplicationBuilderExtensions.UsePathBaseFromConfiguration(null, ""));
+            Assert.Equal("builder", exception.ParamName);
+        }
+
+        [Fact]
+        public void UsePathBaseFromConfiguration_ConfigSectionNameIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new ApplicationBuilder(null).UsePathBaseFromConfiguration(null));
+            Assert.Equal("Missing configSectionName.", exception.Message);
+        }
+    }
+}
---
+++ tests/unit/GiG.Core.Web.Tests.Unit/Mock/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using GiG.Core.Web.Mock.Extensions;
+using System;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.Web.Tests.Unit.Mock
+{
+    [Trait("Category", "Unit")]
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void ConfigureForwardedHeaders_ServiceCollectionIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => ServiceCollectionExtensions.AddMockCorrelationAccessor(null));
+            Assert.Equal("services", exception.ParamName);
+
+            exception = Assert.Throws<ArgumentNullException>(() => ServiceCollectionExtensions.AddMockRequestContextAccessor(null));
+            Assert.Equal("services", exception.ParamName);
+
+            exception = Assert.Throws<ArgumentNullException>(() => ServiceCollectionExtensions.AddMockTenantAccessor(null));
+            Assert.Equal("services", exception.ParamName);
+        }
+    }
+}
