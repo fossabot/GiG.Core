4db17ab6a6f9f3f7a80c236c6a8d802b488b577d
David Anastasi <david.anastasi@igamingcloud.com>
2019-11-19 19:27:30 +0100

    Minor cleanup and more unit tests.

---
+++ tests/unit/GiG.Core.DistributedTracing.Tests.Unit/MassTransit/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using GiG.Core.DistributedTracing.MassTransit.Extensions;
+using System;
+using Xunit;
+
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.DistributedTracing.Tests.Unit.MassTransit
+{
+    [Trait("Category", "Unit")]
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddCorrelationAccessor_ServiceCollectionIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => ServiceCollectionExtensions.AddCorrelationAccessor(null));
+            Assert.Equal("services", exception.ParamName);
+        }
+    }
+}
---
+++ tests/unit/GiG.Core.Logging.Tests.Unit/LoggingConfigurationTests.cs
@@ -0,0 +1,27 @@
+﻿using GiG.Core.Logging.Extensions;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Configuration;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.Logging.Tests.Unit
+{
+    [Trait("Category", "Unit")]
+    public class LoggingConfigurationTests
+    {
+        [Fact]
+        public void ConfigureLogging_HostBuilderIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ConfigurationErrorsException>(() =>
+                Host.CreateDefaultBuilder()
+                .ConfigureLogging(null, "Logging")
+                    .Build());
+            Assert.Equal("Configuration section 'Logging' is not valid", exception.Message);
+        }
+
+
+
+
+    }
+}
