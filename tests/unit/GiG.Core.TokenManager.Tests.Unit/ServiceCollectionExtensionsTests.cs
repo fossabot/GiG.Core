adc74a3de35e7d3c9623de2cd1ae81d91bfd2a4b
David Anastasi <david.anastasi@igamingcloud.com>
2019-11-26 15:19:48 +0100

    Added GiG.Core.TokenManager.Tests.Unit project.

---
+++ tests/unit/GiG.Core.TokenManager.Tests.Unit/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,18 @@
+using GiG.Core.TokenManager.Extensions;
+using System;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.TokenManager.Tests.Unit
+{
+    [Trait("Category", "Unit")]
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddTokenManager_ServiceCollectionIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => ServiceCollectionExtensions.AddTokenManager(null));
+            Assert.Equal("services", exception.ParamName);
+        }
+    }
+}
