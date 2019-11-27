4f55d6f972ee922afc1f1d47b2975a8b183c00f6
David Anastasi <david.anastasi@igamingcloud.com>
2019-11-27 10:11:07 +0100

    Added XML comments and updated README.md

--- src/GiG.Core.TokenManager/Models/TokenClientOptions.cs
+++ src/GiG.Core.TokenManager/Models/TokenClientOptions.cs
@@ -1,32 +1,32 @@
 ﻿namespace GiG.Core.TokenManager.Models
 {
     /// <summary>
-    /// 
+    /// Token Client Options.
     /// </summary>
     public class TokenClientOptions
     {
         /// <summary>
-        /// 
+        /// The Authority URL.
         /// </summary>
         public string AuthorityUrl { get; set; }
 
         /// <summary>
-        /// 
+        /// The Client Id.
         /// </summary>
         public string ClientId { get; set; }
 
         /// <summary>
-        /// 
+        /// The Client Secret.
         /// </summary>
         public string ClientSecret { get; set; }
 
         /// <summary>
-        /// 
+        /// The Scopes on which to grant access.
         /// </summary>
         public string Scopes { get; set; } = "openid";
 
         /// <summary>
-        /// 
+        /// Require HTTPS.
         /// </summary>
         public bool RequireHttps { get; set; } = true;
 
--- src/GiG.Core.TokenManager/Models/TokenManagerOptions.cs
+++ src/GiG.Core.TokenManager/Models/TokenManagerOptions.cs
@@ -1,27 +1,27 @@
 ﻿﻿namespace GiG.Core.TokenManager.Models
 {
     /// <summary>
-    /// 
+    /// Token Manager Options.
     /// </summary>
     public class TokenManagerOptions
     {
         /// <summary>
-        /// 
+        /// The Default configuration  section name 
         /// </summary>
         public const string DefaultSectionName = "TokenManager";
         
         /// <summary>
-        /// 
+        /// The username.
         /// </summary>
         public string Username { get; set; }
 
         /// <summary>
-        /// 
+        /// The password.
         /// </summary>
         public string Password { get; set; }
         
         /// <summary>
-        /// 
+        /// The client options to use.
         /// </summary>
         public TokenClientOptions Client { get; set; } = new TokenClientOptions();
     }
