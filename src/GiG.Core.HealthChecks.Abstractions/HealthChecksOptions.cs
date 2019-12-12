7ff1926e0d1ee38430e64ac2212c4d11e515bed0
John Debono <john.debono@gig.com>
2019-12-12 11:55:46 +0100

    Added Constant Removed Check From Extension Method For HealthCheck Section

--- src/GiG.Core.HealthChecks.Abstractions/HealthChecksOptions.cs
+++ src/GiG.Core.HealthChecks.Abstractions/HealthChecksOptions.cs
@@ -13,25 +13,25 @@
         /// <summary>
         /// The Url for the Ready + Live Health Checks.
         /// </summary>
-        public string CombinedUrl { get; set; } = "/actuator/health";
+        public string CombinedUrl { get; set; } = Constants.CombinedHealthCheckUrl;
 
         /// <summary>
         /// The Url for the Live HealthCheck.
         /// </summary>
-        public string LiveUrl { get; set; }
+        public string LiveUrl { get; set; } = string.Concat(Constants.CombinedHealthCheckUrl, "/", Constants.LiveTag);
 
         /// <summary>
         /// The Url for the Ready HealthCheck.
         /// </summary>
-        public string ReadyUrl { get; set; }
+        public string ReadyUrl { get; set; } = string.Concat(Constants.CombinedHealthCheckUrl, "/", Constants.ReadyTag);
 
         /// <summary>
         /// Constructor.
         /// </summary>
         public HealthChecksOptions()
         {
-            LiveUrl = $"{CombinedUrl}/live";
-            ReadyUrl = $"{CombinedUrl}/ready";
+            LiveUrl = string.Concat(Constants.CombinedHealthCheckUrl, "/", Constants.LiveTag);
+            ReadyUrl = string.Concat(Constants.CombinedHealthCheckUrl, "/", Constants.ReadyTag);
         }
     }
 }
