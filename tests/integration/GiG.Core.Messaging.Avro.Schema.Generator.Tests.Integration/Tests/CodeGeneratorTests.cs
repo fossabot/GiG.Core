510eb25d5477bff49df9bb2c5c0a1886ab097406
david.phyall <david.phyall@igamingcloud.com>
2019-12-23 15:34:43 +0100

    Changed path delimiter to work on Linux.

--- tests/integration/GiG.Core.Messaging.Avro.Schema.Generator.Tests.Integration/Tests/CodeGeneratorTests.cs
+++ tests/integration/GiG.Core.Messaging.Avro.Schema.Generator.Tests.Integration/Tests/CodeGeneratorTests.cs
@@ -20,7 +20,7 @@ namespace GiG.Core.Messaging.Avro.Schema.Generator.Tests.Integration.Tests
             Assembly assembly = typeof(CodeGeneratorTests).Assembly;
             var assemblyName = assembly.GetName().Name;
             var projectPath = assembly.Location.Substring(0, assembly.Location.IndexOf(assemblyName));
-            projectPath = projectPath + assemblyName + @"\" + assemblyName + ".csproj";
+            projectPath = projectPath + assemblyName + @"/" + assemblyName + ".csproj";
 
             // Delete previous avro files - comment this out when debugging
             DeleteAvroFiles(projectPath);
