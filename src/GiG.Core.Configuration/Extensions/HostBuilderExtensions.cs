449b42749051683b7aa868c92bcb3f62e39f0770
jonathan.micallef <jonathan.micallef@gig.com>
2020-01-02 13:31:45 +0100

    Modified the current implementation to scan and read ConfigMaps from folder (./configs) and load JSON files
    Updated tests to test no files, single file, multiple files

--- src/GiG.Core.Configuration/Extensions/HostBuilderExtensions.cs
+++ src/GiG.Core.Configuration/Extensions/HostBuilderExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 
 namespace GiG.Core.Configuration.Extensions
 {
@@ -11,17 +12,25 @@ namespace GiG.Core.Configuration.Extensions
     public static class HostBuilderExtensions
     {
         /// <summary>
-        /// Adds external configuration via JSON File and Environment Variables.
+        /// Adds external configuration files (.json files located in './configs' folder) and Environment Variables.
         /// </summary>
         /// <param name="builder">The <see cref="IHostBuilder"/>.</param>
         /// <returns>The <see cref="IHostBuilder"/>.</returns>
         public static IHostBuilder ConfigureExternalConfiguration([NotNull] this IHostBuilder builder)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
-
+            
             return builder.ConfigureAppConfiguration(appConfig =>
             {
-                appConfig.AddJsonFile("appsettings.override.json", true, true);
+                if (Directory.Exists("./configs"))
+                {
+                    var configFiles = Directory.GetFiles("./configs", "*.json");
+
+                    foreach (var configFile in configFiles)
+                    {
+                        appConfig.AddJsonFile(configFile, true, true);
+                    }
+                }
                 appConfig.AddEnvironmentVariables();
             });
         }
--- tests/integration/GiG.Core.Configuration.Tests.Integration/Tests/HostBuilderExtensionsTests.cs
+++ tests/integration/GiG.Core.Configuration.Tests.Integration/Tests/HostBuilderExtensionsTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 using Xunit;
 
 namespace GiG.Core.Configuration.Tests.Integration.Tests
@@ -34,16 +35,61 @@ namespace GiG.Core.Configuration.Tests.Integration.Tests
             // Act - Assert
             Assert.Equal(expected, configuration.GetValue<string>(ConfigurationSectionName));           
         }
-
+        
         [Fact]
-        public void GetValue_ConfigurationWithAppSettingsOverride_ReturnsOverride()
+        public void GetValue_ConfigurationWithAppSettingsOverrideNoFiles_ReturnsOverride()
         {
             // Arrange
-            const string expected = "Override";
-                    
+            const string expected = "AppSettings";
+            
+            // Arrange
             var host = Host.CreateDefaultBuilder()
                 .ConfigureExternalConfiguration()
                 .Build();
+            
+            var configuration = host.Services.GetRequiredService<IConfiguration>();
+            
+            // Act - Assert
+            Assert.Equal(expected, configuration.GetValue<string>(ConfigurationSectionName));
+            
+        }
+
+        [Fact]
+        public void GetValue_ConfigurationWithAppSettingsOverrideOneFile_ReturnsOverride()
+        {
+            // Arrange
+            const string expected = "Override";
+            
+            File.WriteAllText(Path.Combine("./configs", "appsettings.override.json"), @"{ ""Configuration"": ""Override"" }");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureExternalConfiguration()
+                .Build();
+            
+            File.Delete("./configs/appsettings.override.json");
+            
+            var configuration = host.Services.GetRequiredService<IConfiguration>();
+            
+            // Act - Assert
+            Assert.Equal(expected, configuration.GetValue<string>(ConfigurationSectionName));
+            
+        }
+        
+        [Fact]
+        public void GetValue_ConfigurationWithAppSettingsOverrideTwoFiles_ReturnsOverride()
+        {
+            // Arrange
+            const string expected = "Override2";
+            
+            File.WriteAllText(Path.Combine("./configs", "appsettings.override.json"), @"{ ""Configuration"": ""Override"" }");
+            File.WriteAllText(Path.Combine("./configs", "appsettings.override2.json"), @"{ ""Configuration"": ""Override2"" }");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureExternalConfiguration()
+                .Build();
+            
+            File.Delete("./configs/appsettings.override.json");
+            File.Delete("./configs/appsettings.override2.json");
             
             var configuration = host.Services.GetRequiredService<IConfiguration>();
 
