1a48d53fd97234754fa79cac96aae1b2ebb0629a
Jonathan Busuttil <jonathan.busuttil@gig.com>
2020-01-20 15:59:54 +0100

    [GIGCORE-1128] - added baggage enricher and field transformation in fluentd

---
+++ src/GiG.Core.Logging.Enrichers.DistributedTracing/Internal/ActivityContextEnricher.cs
@@ -0,0 +1,30 @@
+﻿using GiG.Core.DistributedTracing.Abstractions;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace GiG.Core.Logging.Enrichers.DistributedTracing.Internal
+{
+    internal class ActivityContextEnricher : ILogEventEnricher
+    {
+        private readonly IActivityContextAccessor _activityContextAccessor;
+
+        public ActivityContextEnricher(IActivityContextAccessor activityContextAccessor)
+        {
+            _activityContextAccessor = activityContextAccessor;
+        }
+        
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            AddBaggage(logEvent);
+        }
+
+     
+        private void AddBaggage(LogEvent logEvent)
+        {
+            foreach (var baggageEntry in _activityContextAccessor.Baggage)
+            {
+                logEvent.AddPropertyIfAbsent(new LogEventProperty($"baggage.{baggageEntry.Key}", new ScalarValue(baggageEntry.Value)));
+            }
+        }
+    }
+}
