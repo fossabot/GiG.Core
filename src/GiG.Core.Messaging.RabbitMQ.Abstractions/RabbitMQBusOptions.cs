8605e6d0eea70e870ad964dbbcdc631436ed5c62
Shaun Tabone <shaun.tabone@gig.com>
2020-02-06 08:53:38 +0100

    Fixed Orleans Dashboard and added defaults for RabbitMQ options

--- src/GiG.Core.Messaging.RabbitMQ.Abstractions/RabbitMQBusOptions.cs
+++ src/GiG.Core.Messaging.RabbitMQ.Abstractions/RabbitMQBusOptions.cs
@@ -13,27 +13,27 @@
         /// <summary>
         /// The RabbitMQ host name to connect to.
         /// </summary>
-        public string Host { get; set; }
+        public string Host { get; set; } = "localhost";
 
         /// <summary>
         /// The RabbitMQ port to connect to.
         /// </summary>
-        public int Port { get; set; }
+        public int Port { get; set; } = 5672;
 
         /// <summary>
         /// The virtual host for the connection.
         /// </summary>
-        public string VirtualHost { get; set; }
+        public string VirtualHost { get; set; } = "/";
 
         /// <summary>
         /// The Username for connecting to the host.
         /// </summary>
-        public string Username { get; set; }
+        public string Username { get; set; } = "guest";
 
         /// <summary>
         /// The password for connecting to the host.
         /// </summary>
-        public string Password { get; set; }
+        public string Password { get; set; } = "guest";
 
         /// <summary>
         /// The heartbeat interval (in seconds) to keep the host connection alive.
