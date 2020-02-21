b0c89a4ab41c736e1f81b7587fe69dce46898526
CORP\clinton.mifsud <clintonmifsud@gig.com>
2020-02-21 15:04:01 +0100

    Added Reminders Extension For Npgsql

---
+++ docs/GiG.Core.Orleans.Reminders.Npgsql.md
@@ -0,0 +1,63 @@
+# GiG.Core.Orleans.Reminders.Npgsql
+
+This Library provides an API to register PostgreSQL as a Reminder Storage Provider.
+
+## Pre-requisites
+
+The following packages are required to consume this package:
+ - [GiG.Core.Orleans.Silo](GiG.Core.Orleans.Silo.md)
+ - Microsoft.Orleans.Server
+ 
+## Basic Usage
+
+The below code needs to be added to the `Program.cs` when creating a new HostBuilder. This will register an Orleans Silo with PostgreSQL Reminder Storage provider named 'NpgsqlProvider'.
+
+**Note**: Multiple named Npgsql Reminder Storage providers can be added as long as the Provider Name is unique.
+
+**Note**: The `UseOrleans` extension can be found in the nuget package ```Microsoft.Orleans.Server```
+**Note**: The `ConfigureCluster` and `ConfigureEndpoints` extensions can be found in the nuget package ```GiG.Core.Orleans.Silo```
+
+```csharp
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)                                
+            .ConfigureServices(Startup.ConfigureServices)
+            .UseOrleans(ConfigureOrleans);
+
+    public static void ConfigureOrleans(HostBuilderContext ctx, ISiloBuilder builder)
+    {
+        builder.ConfigureCluster(ctx.Configuration)
+            .ConfigureEndpoints(ctx.Configuration)
+            .AddAssemblies(typeof(Grain))
+			 .UseNpgsqlReminderService("NpgsqlProvider", ctx.Configuration);
+    }
+}
+```
+
+### Configuration
+
+The below table outlines the valid Configurations for [NpgsqlOptions](../src/GiG.Core.Orleans.Reminders.Npgsql/Abstractions/NpgsqlOptions.cs). By default the Configuration for the provider is expected to be under the section "Orleans:Reminders:{InstanceName}". 
+
+| Configuration Name | Type   | Required | Default Value |
+|:-------------------|:-------|:---------|:--------------|
+| ConnectionString   | String | Yes      |               |
+
+#### Sample Configuration
+
+```json
+{
+  "Orleans": {
+    "Reminders": {
+      "SampleDb": {
+        "ConnectionString": "Host=localhost;Username=postgres;Password=postgres;Database=sample"
+      }
+    }
+  }
+}
+```
---
+++ tests/unit/GiG.Core.Orleans.Tests.Unit/Reminders/SiloBuilderExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿using GiG.Core.Orleans.Reminders.Npgsql.Extensions;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Configuration;
+using Xunit;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace GiG.Core.Orleans.Tests.Unit.Reminders
+{
+    [Trait("Category", "Unit")]
+    public class SiloBuilderExtensionsTests
+    {
+        [Fact]
+        public void UseNpgsqlReminderService_SiloBuilderIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => SiloBuilderExtensions.UseNpgsqlReminderService(null, null, configuration: null));
+            Assert.Equal("siloBuilder", exception.ParamName);
+
+            exception = Assert.Throws<ArgumentNullException>(() => SiloBuilderExtensions.UseNpgsqlReminderService(null, null, null));
+            Assert.Equal("siloBuilder", exception.ParamName);
+
+            exception = Assert.Throws<ArgumentNullException>(() => SiloBuilderExtensions.UseNpgsqlReminderService(null, null, null, null));
+            Assert.Equal("siloBuilder", exception.ParamName);
+        }
+
+        [Fact]
+        public void UseNpgsqlReminderService_ConfigurationIsNull_ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() =>
+                            Host.CreateDefaultBuilder().UseOrleans(sb => sb.UseNpgsqlReminderService(null, configuration: null)).Build());
+            Assert.Equal("configuration", exception.ParamName);
+
+            exception = Assert.Throws<ArgumentNullException>(() =>
+                            Host.CreateDefaultBuilder().UseOrleans(sb => sb.UseNpgsqlReminderService(storageName: null, null, null)).Build());
+            Assert.Equal("configuration", exception.ParamName);
+        }
+
+        [Fact]
+        public void UseNpgsqlReminderService_ConfigurationSectionIsNull_ThrowsConfigurationErrorsException()
+        {
+            var exception = Assert.Throws<ConfigurationErrorsException>(() =>
+                            Host.CreateDefaultBuilder().UseOrleans(sb => sb.UseNpgsqlReminderService(null, null)).Build());
+            Assert.Equal("Configuration section '' is incorrect.", exception.Message);
+        }
+
+        [Fact]
+        public void UseNpgsqlReminderService_StorageNameIsNull_ThrowsArgumentException()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => Host.CreateDefaultBuilder()
+                .UseOrleans((ctx, x) =>
+                {
+                    x.UseNpgsqlReminderService("", ctx.Configuration, "CustomStorageSection");
+                }).Build());
+            Assert.Equal("'storageName' must not be null, empty or whitespace. (Parameter 'storageName')", exception.Message);
+        }
+
+        [Fact]
+        public void UseNpgsqlReminderService_SectionNameIsNull_ThrowsArgumentException()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => Host.CreateDefaultBuilder()
+                .UseOrleans((ctx, x) =>
+                {
+                    x.UseNpgsqlReminderService("testDB2", ctx.Configuration, "");
+                }).Build());
+            Assert.Equal("'sectionName' must not be null, empty or whitespace. (Parameter 'sectionName')", exception.Message);
+        }
+    }
+}
