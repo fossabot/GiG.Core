25a8c3f61349bfff49407df5ee7616e1dd1f70e5
Jonathan Busuttil <jonathan.busuttil@gig.com>
2020-02-25 09:44:59 +0100

    [GIGCORE3524] - added structured logging option using elastic search formatter in console logging

--- src/GiG.Core.Logging.Sinks.Console/Internal/ConsoleLoggingSinkProvider.cs
+++ src/GiG.Core.Logging.Sinks.Console/Internal/ConsoleLoggingSinkProvider.cs
@@ -1,14 +1,24 @@
 using GiG.Core.Logging.Abstractions;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Configuration;
+using Serilog.Formatting.Elasticsearch;
 
 namespace GiG.Core.Logging.Sinks.Console.Internal
 {
     internal class ConsoleLoggingSinkProvider : ILoggingSinkProvider
     {
+        private readonly ConsoleSinkOptions _sinkOptions;
+        
+        public ConsoleLoggingSinkProvider(IConfiguration configurationSection)
+        {
+            _sinkOptions = configurationSection.Get<ConsoleSinkOptions>() ?? new ConsoleSinkOptions();
+        }
+        
         public void RegisterSink(LoggerSinkConfiguration sinkConfiguration)
         {
-            sinkConfiguration.Console();
+            if (_sinkOptions.UseStructuredLogging) sinkConfiguration.Console(new ElasticsearchJsonFormatter());
+            else sinkConfiguration.Console();
         }
     }
 }
