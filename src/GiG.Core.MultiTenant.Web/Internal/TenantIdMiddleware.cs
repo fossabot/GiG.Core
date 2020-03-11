d75f35725063cc53406cf1ea0295dcd772577c91
Jonathan Busuttil <jonathan.busuttil@gig.com>
2020-03-11 10:40:35 +0100

    [GGCORE-3230] - added tenant id middleware and test

---
+++ src/GiG.Core.MultiTenant.Web/Internal/TenantIdMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using GiG.Core.MultiTenant.Abstractions;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiG.Core.MultiTenant.Web.Internal
+{
+    internal class TenantIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TenantIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var tenantIds = context.Request?.Headers?.TryGetValue(Constants.Header, out var value) != true
+                ? new List<string>()
+                : value.Where(x => !string.IsNullOrWhiteSpace(x));
+            
+            foreach (var tenant in tenantIds.Where(x => !string.IsNullOrWhiteSpace(x)))
+            {
+                Activity.Current.AddBaggage(Constants.TenantIdBaggageKey, tenant);
+            }
+
+            await _next(context);
+        }
+    }
+}
