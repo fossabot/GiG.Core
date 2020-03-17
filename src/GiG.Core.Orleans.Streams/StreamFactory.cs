52e94f75d8935aeb98ba66cb6708f9b41a468612
david.phyall <david.phyall@igamingcloud.com>
2020-03-17 14:46:10 +0100

    Changes related to Telemetry for Streams

--- src/GiG.Core.Orleans.Streams/StreamFactory.cs
+++ src/GiG.Core.Orleans.Streams/StreamFactory.cs
@@ -1,5 +1,7 @@
 ﻿using GiG.Core.DistributedTracing.Abstractions;
 using GiG.Core.Orleans.Streams.Abstractions;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Trace.Configuration;
 using Orleans.Streams;
 using System;
 
@@ -12,14 +14,17 @@ namespace GiG.Core.Orleans.Streams
     public class StreamFactory : IStreamFactory
     {
         private readonly IActivityContextAccessor _activityContextAccessor;
+        private readonly TracerFactory _tracerFactory;
 
         /// <summary>
         /// Constructor.
         /// </summary>
         /// <param name="activityContextAccessor">The <see cref="IActivityContextAccessor" /> to use to add activity id within RequestContext.</param>
-        public StreamFactory(IActivityContextAccessor activityContextAccessor)
+        /// <param name="tracerFactory">The <see cref="TracerFactory"/> used to get the <see cref="Tracer"/> used for Telemetry.</param>
+        public StreamFactory(IActivityContextAccessor activityContextAccessor, TracerFactory tracerFactory = null)
         {
             _activityContextAccessor = activityContextAccessor;
+            _tracerFactory = tracerFactory;
         }
 
         /// <summary>
@@ -34,7 +39,7 @@ namespace GiG.Core.Orleans.Streams
         {
             var stream = streamProvider.GetStream<TMessage>(streamId, streamNameSpace);
           
-            return new Stream<TMessage>(stream, _activityContextAccessor);
+            return new Stream<TMessage>(stream, _activityContextAccessor, _tracerFactory);
         }
     }
 }
