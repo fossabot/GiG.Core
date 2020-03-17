8b750e0d2e521b4420b0a62e8c9dc6444b121f5a
Jonathan Busuttil <jonathan.busuttil@gig.com>
2020-03-17 15:13:27 +0100

    [GIGCORE-3798] - added docker for performance app and tested with local etcd with docker compose

--- tests/performance/GiG.Core.Data.KVStores.Etcd.Tests.Performance/EtcdConfiguration.cs
+++ tests/performance/GiG.Core.Data.KVStores.Etcd.Tests.Performance/EtcdConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public class EtcdConfiguration
     {
-        public string ConnectionString { get; set; } = "http://localhost:2379";
+        public string ConnectionString { get; set; } = "http://etcd:2379";
         public int Port { get; set; } = 2379;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
