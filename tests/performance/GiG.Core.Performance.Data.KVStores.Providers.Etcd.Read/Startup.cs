babb40ee4c90e7c5e1aa47907bff4dae7fb5a023
Jonathan Busuttil <jonathan.busuttil@gig.com>
2020-03-18 14:30:55 +0100

    [GIGCORE-3798] - pr feedback

--- tests/performance/GiG.Core.Performance.Data.KVStores.Providers.Etcd.Read/Startup.cs
+++ tests/performance/GiG.Core.Performance.Data.KVStores.Providers.Etcd.Read/Startup.cs
@@ -35,11 +35,7 @@ namespace GiG.Core.Performance.Data.KVStores.Providers.Etcd.Read
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
