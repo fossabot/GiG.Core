93d465d1cf0b068edbe0d01a9ae1e41069714d0f
Jonathan Busuttil <jonathan.busuttil@gig.com>
2020-04-02 08:55:39 +0200

    added logging performance aspnet app

---
+++ tests/performance/GiG.Core.Performance.Logging/Controllers/LogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GiG.Core.Performance.Logging.Controllers
+{
+    [ApiController]
+    [Route("v{version:apiVersion}/log")]
+    [ApiVersion("1")]
+     public class LogController : ControllerBase
+     {
+         private readonly ILogger<LogController> _logger;
+         private static string LogText = "This is a test log";
+         
+         public LogController(ILogger<LogController> logger)
+         {
+             _logger = logger;
+         }
+         
+         [HttpPost]
+         public void Post()
+         {
+             _logger.LogInformation(LogText);
+         }
+     }
+}
