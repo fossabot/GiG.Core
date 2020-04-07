0e67226e0be418774407c4bd197c2d8f18c99ef0
Shaun Tabone <shaun.tabone@gig.com>
2020-04-07 09:20:39 +0200

    Fixed inline XML docs

--- src/GiG.Core.Data.KVStores/Extensions/ServiceCollectionExtensions.cs
+++ src/GiG.Core.Data.KVStores/Extensions/ServiceCollectionExtensions.cs
@@ -16,7 +16,7 @@ namespace GiG.Core.Data.KVStores.Extensions
         /// </summary>
         /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the services to.</param>
         /// <param name="configureKVStore">A delegate that is used to configure a <see cref="IKVStoreBuilder{T}"/>.</param>
-        /// <returns>The <see cref="IKVStoreBuilder{T}" /> so that additional calls can be chained.</returns>
+        /// <returns>The <see cref="IServiceCollection" /> so that additional calls can be chained.</returns>
         public static IServiceCollection AddKVStores<T>([NotNull] this IServiceCollection services, [NotNull] Action<IKVStoreBuilder<T>> configureKVStore)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
