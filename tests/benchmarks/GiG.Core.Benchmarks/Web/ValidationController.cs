0675e2cffbf4560f72240b74da71547ed4ef25a1
Shaun Tabone <shaun.tabone@gig.com>
2020-04-12 21:19:08 +0200

    Added larger test for validations

--- tests/benchmarks/GiG.Core.Benchmarks/Web/ValidationController.cs
+++ tests/benchmarks/GiG.Core.Benchmarks/Web/ValidationController.cs
@@ -19,8 +19,16 @@ namespace GiG.Core.Benchmarks.Web
         {
             new ValidationFailure("Name", "Name field should contain at least 2 characters."),
             new ValidationFailure("Name", "Name field cannot contain only numbers."),
+            new ValidationFailure("Name", "Name field should contain at most 100 characters."),
             new ValidationFailure("Surname", "Surname field is required."),
-            new ValidationFailure("Address", "Address field is required.")
+            new ValidationFailure("Surname", "Surname field should contain at least 2 characters."),
+            new ValidationFailure("Surname", "Surname field cannot contain only numbers."),
+            new ValidationFailure("Surname", "Surname field should contain at most 100 characters."),
+            new ValidationFailure("Address", "Address field is required."),
+            new ValidationFailure("Address", "Address field should contain at least 2 characters."),
+            new ValidationFailure("Address", "Address field should contain at most 500 characters."),
+            new ValidationFailure("Age", "Age field should be greater than or equal 18."),
+            new ValidationFailure("Age", "Age field should be less than or equal 100.")
         });
     }
 }
