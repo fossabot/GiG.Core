bbb8fb4e5070f646cbb752bd1ded1883571929dd
Shaun Tabone <shaun.tabone@gig.com>
2020-04-16 18:21:33 +0200

    Cleanup

--- src/GiG.Core.Orleans.Streams.Abstractions/NamespacePredicate.cs
+++ src/GiG.Core.Orleans.Streams.Abstractions/NamespacePredicate.cs
@@ -1,26 +1,7 @@
-using Orleans;
 using Orleans.Streams;
-using System;
 
 namespace GiG.Core.Orleans.Streams.Abstractions
 {
-    /// <summary>
-    /// The <see cref="NamespaceImplicitStreamSubscriptionAttribute"/> attribute is used to mark grains as implicit stream
-    /// subscriptions by filtering stream namespaces to subscribe using a stream namespaces with prefix.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-    public sealed class NamespaceImplicitStreamSubscriptionAttribute : ImplicitStreamSubscriptionAttribute
-    {
-        /// <summary>
-        /// The stream namespace to subscribe to.
-        /// </summary>
-        /// <param name="streamNamespace">The stream namespace.</param>
-        public NamespaceImplicitStreamSubscriptionAttribute(string streamNamespace)
-            : base(new NamespacePredicate(streamNamespace))
-        {
-        }
-    }
-    
     /// <summary>
     /// <see cref="IStreamNamespacePredicate"/> implementation allowing to filter stream namespaces with prefix.
     /// </summary>
