3e1ec6d2674c7f67709a25fecf0293a6d88fbefe
Shaun Tabone <shaun.tabone@gig.com>
2020-04-16 20:18:56 +0200

    Fixed inline doc

--- src/GiG.Core.HealthChecks.Orleans.Streams.Kafka/HealthChecksBuilderExtensions.cs
+++ src/GiG.Core.HealthChecks.Orleans.Streams.Kafka/HealthChecksBuilderExtensions.cs
@@ -23,8 +23,8 @@ namespace GiG.Core.HealthChecks.Orleans.Streams.Kafka
         /// </summary>
         /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
         /// <param name="configuration">The <see cref="IConfiguration"/>.</param>
-        /// <param name="topic">The topic name to produce kafka messages on. Optional. If <c>null</c> the topic default 'healthcheck-topic' will be used for the name.</param>
-        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'kafka' will be used for the name.</param>
+        /// <param name="topic">The topic name to produce kafka messages on. Optional.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'KafkaOrleansStreams' will be used for the name.</param>
         /// <param name="failureStatus">
         /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
         /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
@@ -47,8 +47,8 @@ namespace GiG.Core.HealthChecks.Orleans.Streams.Kafka
         /// </summary>
         /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
         /// <param name="configurationSection">The <see cref="IConfigurationSection"/>.</param>
-        /// <param name="topic">The topic name to produce kafka messages on. Optional. If <c>null</c> the topic default 'healthcheck-topic' will be used for the name.</param>
-        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'kafka' will be used for the name.</param>
+        /// <param name="topic">The topic name to produce kafka messages on. Optional.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'KafkaOrleansStreams' will be used for the name.</param>
         /// <param name="failureStatus">
         /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
         /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
