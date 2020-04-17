73553e1726256d54f86463a7082cf2f1f44e9565
John Debono <john.debono@gig.com>
2020-04-17 09:52:38 +0200

    Added GiG.Core.Logging.AspNetCore Project

---
+++ src/GiG.Core.Logging.AspNetCore.Http/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace GiG.Core.Logging.AspNetCore.Http.Extensions
+{
+    /// <summary>
+    /// The <see cref="IApplicationBuilder" /> Extensions.
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds Logging for Http Request and Http Response.
+        /// </summary>
+        /// <param name="builder">The <see cref="IApplicationBuilder" />.</param>
+        /// <returns>The <see cref="IApplicationBuilder" />.</returns>
+        public static IApplicationBuilder UseHttpRequestResponseLogging([NotNull] this IApplicationBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            return builder.UseMiddleware<HttpRequestResponseLoggingMiddleware>();
+        }
+    }
+}
