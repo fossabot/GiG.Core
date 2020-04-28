2665811db17cfc61adb36543d03752c3d9c02c67
david.phyall <david.phyall@igamingcloud.com>
2020-04-28 15:14:24 +0200

    Added etcd Watch Range Performance test
    Added etcd Get Range Performance tests Endpoints

---
+++ tests/performance/GiG.Core.Performance.Data.KVStores.Providers.Etcd.WatchRange/Program.cs
@@ -0,0 +1,18 @@
+﻿using GiG.Core.Logging.All.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace GiG.Core.Performance.Data.KVStores.Providers.Etcd.WatchRange
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging()
+                .ConfigureServices(Startup.ConfigureServices);
+    }
+}
