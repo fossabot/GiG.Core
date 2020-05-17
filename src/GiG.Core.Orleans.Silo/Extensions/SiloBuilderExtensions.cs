9205c082792e4f9710d50e8951ede99120fb0b50
Shaun Tabone <shaun.tabone@gig.com>
2020-05-17 08:44:04 +0200

    Added PubSub type configuration

--- src/GiG.Core.Orleans.Silo/Extensions/SiloBuilderExtensions.cs
+++ src/GiG.Core.Orleans.Silo/Extensions/SiloBuilderExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using GiG.Core.Orleans.Clustering.Abstractions;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
+using Orleans.Streams;
 using System;
 using System.Configuration;
 using System.Linq;
@@ -25,7 +27,8 @@ namespace GiG.Core.Orleans.Silo.Extensions
         /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
         /// <param name="assemblies">The <see cref="Assembly"/> array which will be added to the Silo.</param>
         /// <returns>Returns the <see cref="ISiloBuilder"/> so that more methods can be chained.</returns>
-        public static ISiloBuilder AddAssemblies([NotNull] this ISiloBuilder builder, [NotNull] params Assembly[] assemblies)
+        public static ISiloBuilder AddAssemblies([NotNull] this ISiloBuilder builder,
+            [NotNull] params Assembly[] assemblies)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
             if (assemblies == null) throw new ArgumentNullException(nameof(assemblies));
@@ -69,10 +72,13 @@ namespace GiG.Core.Orleans.Silo.Extensions
         /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
         /// <param name="configurationSection">The <see cref="IConfigurationSection"/> which binds to <see cref="ClusterOptions"/>.</param>
         /// <returns>Returns the <see cref="ISiloBuilder"/> so that more methods can be chained.</returns>
-        public static ISiloBuilder ConfigureCluster([NotNull] this ISiloBuilder builder, [NotNull] IConfigurationSection configurationSection)
+        public static ISiloBuilder ConfigureCluster([NotNull] this ISiloBuilder builder,
+            [NotNull] IConfigurationSection configurationSection)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
-            if (configurationSection?.Exists() != true) throw new ConfigurationErrorsException($"Configuration section '{configurationSection?.Path}' is incorrect.");
+            if (configurationSection?.Exists() != true)
+                throw new ConfigurationErrorsException(
+                    $"Configuration section '{configurationSection?.Path}' is incorrect.");
 
             var options = configurationSection.Get<Clustering.Abstractions.ClusterMembershipOptions>();
 
@@ -93,7 +99,8 @@ namespace GiG.Core.Orleans.Silo.Extensions
         /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
         /// <param name="configuration">The <see cref="IConfiguration"/> which binds to <see cref="ClusterOptions"/>.</param>
         /// <returns>Returns the <see cref="ISiloBuilder"/> so that more methods can be chained.</returns>
-        public static ISiloBuilder ConfigureCluster([NotNull] this ISiloBuilder builder, [NotNull] IConfiguration configuration)
+        public static ISiloBuilder ConfigureCluster([NotNull] this ISiloBuilder builder,
+            [NotNull] IConfiguration configuration)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
@@ -107,7 +114,8 @@ namespace GiG.Core.Orleans.Silo.Extensions
         /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
         /// <param name="configuration">The <see cref="IConfiguration"/> which binds to <see cref="EndpointOptions"/>.</param>
         /// <returns>Returns the <see cref="ISiloBuilder"/> so that more methods can be chained.</returns>
-        public static ISiloBuilder ConfigureEndpoints([NotNull] this ISiloBuilder builder, [NotNull] IConfiguration configuration)
+        public static ISiloBuilder ConfigureEndpoints([NotNull] this ISiloBuilder builder,
+            [NotNull] IConfiguration configuration)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
@@ -121,12 +129,13 @@ namespace GiG.Core.Orleans.Silo.Extensions
         /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
         /// <param name="configurationSection">The <see cref="IConfigurationSection"/> which binds to <see cref="EndpointOptions"/>.</param>
         /// <returns>Returns the <see cref="ISiloBuilder"/> so that more methods can be chained.</returns>
-        public static ISiloBuilder ConfigureEndpoints([NotNull] this ISiloBuilder builder, IConfigurationSection configurationSection)
+        public static ISiloBuilder ConfigureEndpoints([NotNull] this ISiloBuilder builder,
+            IConfigurationSection configurationSection)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
-         
+
             var endpointOptions = configurationSection?.Get<EndpointOptions>() ?? new EndpointOptions();
-          
+
             var siloAddress = Dns.GetHostEntry(Dns.GetHostName()).AddressList
                 .First(a => a.AddressFamily == AddressFamily.InterNetwork);
 
@@ -144,7 +153,8 @@ namespace GiG.Core.Orleans.Silo.Extensions
         /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
         /// <param name="configuration">The <see cref="IConfiguration"/> which binds to <see cref="ClusterOptions"/> and <see cref="EndpointOptions"/>.</param>
         /// <returns>Returns the <see cref="ISiloBuilder"/> so that more methods can be chained.</returns>
-        public static ISiloBuilder ConfigureDefaults([NotNull] this ISiloBuilder builder, [NotNull] IConfiguration configuration)
+        public static ISiloBuilder ConfigureDefaults([NotNull] this ISiloBuilder builder,
+            [NotNull] IConfiguration configuration)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
@@ -152,6 +162,25 @@ namespace GiG.Core.Orleans.Silo.Extensions
             return builder
                 .ConfigureCluster(configuration)
                 .ConfigureEndpoints(configuration);
+        }
+
+        /// <summary>
+        /// Configures the Silo PubSub Type for Stream Provider.
+        /// </summary>
+        /// <param name="builder">The Orleans <see cref="ISiloBuilder"/>.</param>
+        /// <param name="streamProviderName">The Stream Provider name.</param>
+        /// <param name="pubSubType">The PubSub Type.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static ISiloBuilder ConfigurePubSubType([NotNull] this ISiloBuilder builder, [NotNull] string streamProviderName, StreamPubSubType pubSubType)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (string.IsNullOrEmpty(streamProviderName)) throw new ArgumentException($"'{nameof(streamProviderName)}' must not be null, empty or whitespace.", nameof(streamProviderName));
+
+            return builder.ConfigureServices(x =>
+                x.Configure<StreamPubSubOptions>(streamProviderName, 
+                    o => o.PubSubType = pubSubType));
         }
     }
 }
